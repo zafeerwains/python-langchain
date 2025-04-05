644eb42249183fae53c9ff43f0ff81347a393a61
zafeerwains <zafeerwains@gmail.com>
2025-04-05 15:56:28 +0000

    minor change i func call

--- fastapi_LLM_call.py
+++ fastapi_LLM_call.py
@@ -1,12 +1,14 @@
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 class ChatRequest(BaseModel):
     query: str
 
-import os
+
 load_dotenv()
 app = FastAPI()
 
@@ -16,12 +18,14 @@ llm = ChatGoogleGenerativeAI(
     google_api_key=os.getenv('GOOGLE_API_KEY')
 )
 
+
 def get_response(query: str) -> str:
     response = llm.invoke(query)
     return response
 
+
 @app.post("/chat")
 async def chat(request: ChatRequest):
-    response = get_response(request.query)
-    return { response.content}
-    
+    response = llm.invoke(request.query)
+    # response = get_response(request.query)
+    return {response.content}
